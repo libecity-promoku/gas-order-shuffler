74dba3997b6ab21bd90901dc13e7085e1db3729e
Kyosuke Takenaka <smith.shimomura@gmail.com>
2023-10-24 08:35:21 +0000

    feat: server/hosting以下にホスティングするものを吐き出すように

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import { viteSingleFile } from 'vite-plugin-singlefile';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteSingleFile()],
+  build: {
+    outDir: "server/hosting",
+  },
 });
